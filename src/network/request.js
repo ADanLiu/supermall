c01d246a7be3f70f6db9a60dff4281dc56b4122b
Adan <1005908609@qq.com>
2021-03-04 10:49:15 +0800

    TabBar,Home components

---
+++ src/network/request.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export function request(config) {
+  //创建axios实例
+  const instance = axios.create({
+    baseURL: "http://106.54.54.237:8000/api/mn",
+    timeout: 5000
+  });
+
+  //axios拦截器
+  //请求拦截
+  instance.interceptors.request.use(config=>{
+    return config;
+  },err=>{
+    // console.log(err);
+  })
+
+  //响应拦截
+  instance.interceptors.response.use(res=>{
+    return res.data;
+  },err=>{
+      console.log(err);
+  })
+
+  //发送真正的网络请求
+  return instance(config);
+}
---
+++ src/views/home/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./chidComps/HomeSwiper";
+import RecommendView from './chidComps/RecommendView';
+
+import { getHomeMultidata } from "network/home";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    //请求多个数据
+    getHomeMultidata().then((res) => {
+      // this.result=res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
+</style>
