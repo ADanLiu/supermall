035bf453a2935e31784d72563889a9efeb7da91f
Adan <1005908609@qq.com>
2021-03-08 13:48:11 +0800

    image

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# supermall
+# supermall：仿蘑菇街手机端项目
+
+### demo地址
+
+----
+
+http://shoppingstreet.space
+
+pc端请使用chrome手机模式预览
+
+### 项目主要用到技术栈
+
+- vue2
+- vue-cli
+- vuex
+- vue-router
+- axios
+- webpack
+- git
+
+### 项目效果
+
+> 地址 : [supermall](http://shoppingstreet.space)
+>
+> 注1：项目采用的数据接口并不是自己开发的，这里只是完成前端开发，项目最终使用Nginx部署在远程centos7服务器上
+>
+> 注2：项目开发时是使用chrome 版本 84.0.（64 位）手机模式进行调试的，其他浏览器暂未测试
+
+#### 购物车
+
+
+
+
+
+
 
 ## Project setup
+
 ```
 npm install
 ```
@@ -13,16 +48,6 @@ npm run serve
 ### Compiles and minifies for production
 ```
 npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
 ```
 
 ### Customize configuration
--- src/network/request.js
+++ src/network/request.js
@@ -3,25 +3,25 @@ import axios from "axios";
 export function request(config) {
   //创建axios实例
   const instance = axios.create({
-    baseURL: "",
+    baseURL: "http://106.54.54.237:8000/api/mn",
     timeout: 5000
   });
 
-  // //axios拦截器
-  // //请求拦截
-  // instance.interceptors.request.use(config=>{
-  //   return config;
-  // },err=>{
-  //   // console.log(err);
-  // })
+  //axios拦截器
+  //请求拦截
+  instance.interceptors.request.use(config=>{
+    return config;
+  },err=>{
+    // console.log(err);
+  })
 
-  // //响应拦截
-  // instance.interceptors.response.use(res=>{
-  //   return res.data;
-  // },err=>{
-  //     console.log(err);
-  // })
+  //响应拦截
+  instance.interceptors.response.use(res=>{
+    return res.data;
+  },err=>{
+      console.log(err);
+  })
 
-  //发送真正的网络请求,return一个promise对象
+  //发送真正的网络请求，return一个promise对象
   return instance(config);
 }
