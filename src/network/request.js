bc53d365dee3f639271fd39cc73ea6129244a078
Adan <1005908609@qq.com>
2021-03-08 11:40:40 +0800

    network

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function request(config) {
   //创建axios实例
   const instance = axios.create({
-    baseURL: "http://106.54.54.237:8000/api/mn",
+    baseURL: "",
     timeout: 5000
   });
 
